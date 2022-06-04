6e19242ca814a7c77e50a3debe80c4d57071865b
jawoznia <jnwznk@gmail.com>
2022-06-04 17:30:43 +0200

    Smaller JSON loaded from file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct Prizes {
+    prizes: Vec<Prize>,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct Prize {
+    year: String,
+    category: String,
+    laureates: Vec<Laureat>,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct Laureat {
+    id: String,
+    firstname: String,
+    surname: String,
+    motivation: String,
+    share: String,
+}
+
 pub fn save_json_input_to_db(db_path: &str, json_path: &str) {
     create_db(db_path);
 
@@ -17,14 +41,53 @@ fn create_db(path: &str) {
         .unwrap();
 }
 
-fn load_json(path: &str) {}
+fn load_json(path: &str) -> Prizes {
+    let json = fs::read_to_string(path).expect("Unable to read file.");
+    println!("{}", json);
+    serde_json::from_str::<Prizes>(json.as_str()).expect("JSON was not well-formatted")
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn create_db() {
-        save_json_input_to_db("dummy.db", "data/prizes.json");
+    fn should_create_db() -> std::io::Result<()> {
+        save_json_input_to_db("dummy.db", "data/prize.json");
+        fs::remove_file("dummy.db")?;
+        Ok(())
+        // todo!();
+    }
+
+    #[test]
+    fn should_loud_dummy_json() {
+        let prizes = load_json("data/single_prize.json");
+        let expected_prize = Prizes {
+            prizes: vec![Prize {
+                year: String::from("2021"),
+                category: String::from("chemistry"),
+                laureates: vec![
+                    Laureat {
+                        id: String::from("1002"),
+                        firstname: String::from("Benjamin"),
+                        surname: String::from("List"),
+                        motivation: String::from(
+                            "\"for the development of asymmetric organocatalysis\"",
+                        ),
+                        share: String::from("2"),
+                    },
+                    Laureat {
+                        id: String::from("1003"),
+                        firstname: String::from("David"),
+                        surname: String::from("MacMillan"),
+                        motivation: String::from(
+                            "\"for the development of asymmetric organocatalysis\"",
+                        ),
+                        share: String::from("2"),
+                    },
+                ],
+            }],
+        };
+        assert_eq!(prizes, expected_prize);
     }
 }
