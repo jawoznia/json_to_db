02c32a0c5f97d4b365957b4ae9d44a660c953df2
jawoznia <jnwznk@gmail.com>
2022-06-04 17:52:04 +0200

    Moved db related code to db_manager.

---
+++ src/db_manager.rs
@@ -0,0 +1,34 @@
+pub fn create_db(path: &str) {
+    let connection = sqlite::open(path).unwrap();
+
+    connection
+        .execute(
+            "
+        CREATE TABLE prizes (year INTEGER, category TEXT);
+        CREATE TABLE laureat (id INTEGER, firstname TEXT, surname TEXT, motivation TEXT, share INTEGER, winning_year INTEGER);
+        ",
+        )
+        .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    fn remove_db_if_present() {
+        match fs::remove_file("dummy.db") {
+            Ok(_) => (),
+            Err(_) => (),
+        }
+    }
+
+    #[test]
+    fn should_create_db() -> std::io::Result<()> {
+        remove_db_if_present();
+        create_db("dummy.db");
+        fs::remove_file("dummy.db")?;
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,11 @@
+mod db_manager;
 mod json_loader;
 
+use db_manager::create_db;
 use json_loader::load_json;
 
 pub fn save_json_input_to_db(db_path: &str, json_path: &str) {
     create_db(db_path);
 
     load_json(json_path);
-}
-
-fn create_db(path: &str) {
-    let connection = sqlite::open(path).unwrap();
-
-    connection
-        .execute(
-            "
-        CREATE TABLE prizes (year INTEGER, category TEXT);
-        CREATE TABLE laureat (id INTEGER, firstname TEXT, surname TEXT, motivation TEXT, share INTEGER, winning_year INTEGER);
-        ",
-        )
-        .unwrap();
-}
-
-#[cfg(test)]
-mod tests {
-    use std::fs;
-
-    use super::*;
-
-    fn remove_db_if_present() {
-        match fs::remove_file("dummy.db") {
-            Ok(_) => (),
-            Err(_) => (),
-        }
-    }
-
-    #[test]
-    fn should_create_db() -> std::io::Result<()> {
-        remove_db_if_present();
-        save_json_input_to_db("dummy.db", "data/prize.json");
-        fs::remove_file("dummy.db")?;
-        Ok(())
-    }
 }
